475fb8c30e3fa64ad2ab59ac4daa83e971941fc5
Shingirai Chakaduka <sheengy24@gmail.com>
2016-11-18 21:53:29 +0530

    Added starter theme

---
+++ wp-content/themes/mu/inc/customizer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * MusicUnites Theme Customizer.
+ *
+ * @package MusicUnites
+ */
+
+/**
+ * Add postMessage support for site title and description for the Theme Customizer.
+ *
+ * @param WP_Customize_Manager $wp_customize Theme Customizer object.
+ */
+function mu_customize_register( $wp_customize ) {
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
+}
+add_action( 'customize_register', 'mu_customize_register' );
+
+/**
+ * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
+ */
+function mu_customize_preview_js() {
+	wp_enqueue_script( 'mu_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+}
+add_action( 'customize_preview_init', 'mu_customize_preview_js' );
