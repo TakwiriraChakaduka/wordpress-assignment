b80426d2c7c35a458a303410b90d9b3f7eb410ee
Shingirai Chakaduka <sheengy24@gmail.com>
2016-11-19 19:34:13 +0530

    Modified Layout & added simple styles

--- wp-content/themes/mu/footer.php
+++ wp-content/themes/mu/footer.php
@@ -11,16 +11,21 @@
 
 ?>
 
-	</div><!-- #content -->
+      </div><!-- #content -->
+    </div>
+  </div>
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mu' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mu' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mu' ), 'mu', '<a href="https://github.com/TakwiriraChakaduka" rel="designer">Shingirai Chakaduka</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+  <div class="footer-area full">
+    <div class="main-page">
+    	<footer id="colophon" class="site-footer inner" role="contentinfo">
+    		<div class="site-info">
+    			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mu' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mu' ), 'WordPress' ); ?></a>
+    			<span class="sep"> | </span>
+    			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mu' ), 'mu', '<a href="https://github.com/TakwiriraChakaduka" rel="designer">Shingirai Chakaduka</a>' ); ?>
+    		</div><!-- .site-info -->
+    	</footer><!-- #colophon -->
+    </div>
+  </div>
 
 <?php wp_footer(); ?>
 
