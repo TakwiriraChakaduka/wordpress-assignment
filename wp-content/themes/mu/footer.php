13a241362a7f273bf811efdda1574fa135a344af
Shingirai Chakaduka <sheengy24@gmail.com>
2016-11-21 00:32:24 +0530

    Modified footer widgets styling

--- wp-content/themes/mu/footer.php
+++ wp-content/themes/mu/footer.php
@@ -18,18 +18,14 @@
   <div class="footer-area full">
     <div class="main-page">
     	<footer id="colophon" class="site-footer inner" role="contentinfo">
-        <div id="footer-widgets">
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-1') ) : ?>
+        <?php endif; ?>
 
-            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-1') ) : ?>
-            <?php endif; ?>
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-2') ) : ?>
+        <?php endif; ?>
 
-            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-2') ) : ?>
-            <?php endif; ?>
-
-            <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-3') ) : ?>
-            <?php endif; ?>
-
-        </div>
+        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('footer-3') ) : ?>
+        <?php endif; ?>
     		<div class="site-info">
     			<p  class="pull-left"><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mu' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'mu' ), 'WordPress' ); ?></a></p>
     			<p class="pull-right"><?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'mu' ), 'mu', '<a href="https://github.com/TakwiriraChakaduka" rel="designer">Shingirai Chakaduka</a>' ); ?></p>
